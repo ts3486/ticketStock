22d6ebf1577fd2c5957c2e5270ad85018aced63d
ts3486 <taos3486@gmail.com>
2022-02-23 02:05:22 +0900

    ticketNFT contract readable

--- client/functions/web3/mintTicket.ts
+++ client/functions/web3/mintTicket.ts
@@ -4,16 +4,8 @@ import detectEthereumProvider from "@metamask/detect-provider";
 const Web3 = require("web3");
 const web3 = new Web3("wss://rinkeby.infura.io/ws/v3/0f3a6fad96f04d13bbbf4654d9099af7");
 const { mnemonic, publicKey, privateKey } = require("../../secret.json");
-const { setupLoader } = require("@openzeppelin/contract-loader");
-
-const provider = detectEthereumProvider();
-
-const loader = setupLoader({
-  provider, // either a web3 provider or a web3 instance
-});
-
-const nftBuild = loader.web3.fromArtifact("TicketNFT");
-const contract = new web3.eth.Contract(nftBuild.abi, nftBuild.networks[4].address);
+const nftBuild = require("../../../build/contracts/TicketNFT.json");
+const contract = new web3.eth.Contract(nftBuild.abi, nftBuild.networks[5777].address);
 // const contract = new web3.eth.Contract(nftBuild.abi, nftBuild.networks[5777].address); for ganache
 
 export const mintTicket = async (account: string, ticketURI: string) => {

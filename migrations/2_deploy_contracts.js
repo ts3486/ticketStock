68508105e1e3cd20fda5278874dbbebab53e823b
ts3486 <taos3486@gmail.com>
2022-02-23 01:14:33 +0900

    added input to contract deploy

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,13 +1,13 @@
 // const SimpleStorage = artifacts.require("SimpleStorage");
 // const TutorialToken = artifacts.require("TutorialToken");
 // const ComplexStorage = artifacts.require("ComplexStorage");
-const Ticket = artifacts.require("Ticket");
 const TicketNFT = artifacts.require("TicketNFT");
+
+const IPFS_IMAGE_METADATA_URI = `https://gateway.pinata.cloud/ipfs/QmUojKBcTFu9vNEGp2AVjPn2t6TXFstBKt9rQHkjjjXMJd/`;
 
 module.exports = function (deployer) {
   // deployer.deploy(SimpleStorage);
   // deployer.deploy(TutorialToken);
   // deployer.deploy(ComplexStorage);
-  deployer.deploy(Ticket);
-  deployer.deploy(TicketNFT);
+  deployer.deploy(TicketNFT, "TicketNFT", "TKT", IPFS_IMAGE_METADATA_URI);
 };
